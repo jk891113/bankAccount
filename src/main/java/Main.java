9ae912146b5a378aa83d336a18d5985e043a1ab4
HanJungkyu <jk891113@naver.com>
2022-11-24 09:53:13 +0900

    MainUI 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        MainUI mainUI = new MainUI();
+        mainUI.run();
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            mainUI.run();
+            int chooseMain = scanner.nextInt();
+            if (chooseMain == 1) {
+
+            } else if (chooseMain == 2) {
+
+            } else if (chooseMain == 3) {
+
+            } else if (chooseMain == 4) {
+
+            } else if (chooseMain == 5) {
+
+            } else if (chooseMain == 6) {
+
+            } else if (chooseMain == 7) {
+
+            } else if (chooseMain == 8) {
+
+            } else if (chooseMain == 9) {
+
+            } else if (chooseMain == 0) {
+
+            }
+        }
     }
 }
---
+++ src/main/java/MainUI.java
@@ -0,0 +1,19 @@
+public class MainUI {
+    public void run() {
+        System.out.println("------------------------------");
+        System.out.println("-----------1.계좌생성-----------");
+        System.out.println("-----------2.계좌수정-----------");
+        System.out.println("-----------3.계좌삭제-----------");
+        System.out.println("------------------------------");
+        System.out.println("-----------4.번호조회-----------");
+        System.out.println("-----------5.이름조회-----------");
+        System.out.println("-----------6.전체조회-----------");
+        System.out.println("------------------------------");
+        System.out.println("-----------7.입금--------------");
+        System.out.println("-----------8.출금--------------");
+        System.out.println("-----------9.잔고확인-----------");
+        System.out.println("------------------------------");
+        System.out.println("-----------0.종료--------------");
+        System.out.println("------------------------------");
+    }
+}

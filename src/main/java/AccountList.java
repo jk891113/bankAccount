60b67d4c4243ce325590889e4cfcf89aaf93921f
Park Seowoo <5w31892p@gmail.com>
2022-11-25 13:46:49 +0900

    return 위치 수정

--- src/main/java/AccountList.java
+++ src/main/java/AccountList.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class AccountList {
     private List<Account> accountList = new ArrayList<>();
-    List<Account> accountList = new ArrayList<>();
 
     // 계좌정보 리스트 생성
     public void addAccount(Account account) {
@@ -83,9 +82,9 @@ public class AccountList {
                         return true;
                     }
                 }
-                return false;
             }
         }
+        return false;
     }
 
     public boolean getAccountNum(String accountNum) {

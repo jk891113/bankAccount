32ebc0c3a10be8abdc78b509f65702af1d0740f7
SangHwan <skyi0909@naver.com>
2022-11-28 16:00:31 +0900

    MainProcess 주석 추가

--- src/main/java/service/MainProcess.java
+++ src/main/java/service/MainProcess.java
@@ -9,8 +9,10 @@ import java.util.regex.Pattern;
 
 public class MainProcess {
     Scanner scanner = new Scanner(System.in);
+    //계좌 정보를 가지고있는 리스트
     AccountList accountList = new AccountList();
 
+    //계좌 생성 기능
     public void createProcess() {
         // 이름 입력
         System.out.print("예금주 : ");
@@ -24,6 +26,7 @@ public class MainProcess {
             System.out.println("계좌번호 형식에 맞게 작성하세요. (ex.000-0000-0000)");
             System.out.print("계좌번호 : ");
             accountNum = scanner.nextLine();
+            // 000-0000-0000 형식에 맞게 입력시 checkNum = true, 아닐시 재입력
             boolean checkNum = Pattern.matches("^\\d{3}-\\d{4}-\\d{4}$", accountNum);
             if (checkNum) {
                 break;
@@ -37,6 +40,7 @@ public class MainProcess {
             System.out.println("4자리 숫자로 작성해주세요.");
             System.out.print("비밀번호 : ");
             password = scanner.nextLine();
+            // 0000 형식에 맞게 입력시 다음 진행, 아닐시 재입력
             boolean checkPassword = Pattern.matches("\\d{4}", password);
             if (checkPassword) {
                 break;
@@ -50,25 +54,28 @@ public class MainProcess {
             try {
                 System.out.print("잔고 : ");
                 String amount = scanner.nextLine();
-                nAmount = Integer.parseInt(amount);
+                nAmount = Integer.parseInt(amount); //예외처리 기능을 사용하기 위해 미리 강제 형태변환
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("잔고 입력이 올바르지 않습니다.");
             }
         }
+        //현재 날짜,시간 입력
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm");
         String date = dateTimeFormatter.format(LocalDateTime.now());
-
+        //계좌 생성정보 저장
         accountList.addAccount(name, bankName, accountNum, password, nAmount, date);
         accountList.showAllAccount();
     }
 
+    //계좌 수정 기능
     public void editProcess() {
         int index;
         // 계좌번호 입력 후 AccountList에서 입력한 계좌번호의 계좌정보 리스트의 인덱스를 받는다.
         while (true) {
             System.out.print("계좌번호 : ");
             String accountNum = scanner.nextLine();
+            // 저장된 계좌에 맞게 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -83,6 +90,7 @@ public class MainProcess {
             String password = scanner.nextLine();
             int exact = accountList.passwordCorrection(index, password);
             if (exact == 1) {
+                // 선택한 계좌의 수정할 이름,은행명,비밀번호 입력
                 System.out.print("수정 이름 : ");
                 String replaceName = scanner.nextLine();
                 System.out.print("수정 은행: ");
@@ -91,6 +99,7 @@ public class MainProcess {
                 while (true) {
                     System.out.print("수정 비밀번호: ");
                     replacePassword = scanner.nextLine();
+                    // 0000 형식에 맞게 입력시 다음 진행, 아닐시 재입력
                     if (!(Pattern.matches("\\d{4}", replacePassword))) {
                         System.out.println("***비밀번호가 입력방식이 올바르지 않습니다.***");
                         continue;
@@ -106,12 +115,14 @@ public class MainProcess {
         }
     }
 
+    //계좌 삭제 기능
     public void deleteProcess() {
         String accountNum;
         int index;
         while (true) {
             System.out.print("계좌번호 : ");
             accountNum = scanner.nextLine();
+            // 저장된 계좌에 맞게 계좌번호 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -124,8 +135,10 @@ public class MainProcess {
             System.out.print("비밀번호 : ");
             String password = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌의 비밀번호에 맞게 입력시 true , 없을시 재입력
             int exact = accountList.passwordCorrection(index, password);
             if (exact == 1) {
+                //선택한 계좌 삭제
                 accountList.deleteAccount(index);
                 break;
             } else {
@@ -134,40 +147,49 @@ public class MainProcess {
         }
     }
 
+    //계좌 조회 기능(계좌번호를 이용한 계좌조회)
     public void checkByAccountNumProcess() {
         int index;
         while (true) {
             System.out.print("계좌번호 : ");
             String accountNum = scanner.nextLine();
+            // 저장된 계좌에 맞게 계좌번호 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
             } else {
+                //계좌 조회
                 accountList.getAccount(index);
                 break;
             }
         }
     }
 
+    //계좌 조회 기능(예금주 이름을 이용한 계좌조회)
     public void checkByNameProcess() {
         int index;
         while (true) {
             System.out.print("예금주 : ");
             String name = scanner.nextLine();
+            // 저장된 예금주 이름에 맞게 입력시 true , 없을시 재입력
             index = accountList.getIndexByName(name);
             if (index == -1) {
                 System.out.println("존재하는 예금주가 없습니다.");
             } else {
+                //계좌 조회
                 accountList.getAccount(index);
                 break;
             }
         }
     }
 
+    //계좌 전체 조회
     public void accountListProcess() {
+        //계좌 조회
         accountList.showAllAccount();
     }
 
+    //계좌 거래내역 조회
     public void historyProcess() {
         String accountNum;
         int index;
@@ -176,6 +198,7 @@ public class MainProcess {
             System.out.print("계좌번호 : ");
             accountNum = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌에 맞게 계좌번호 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -188,8 +211,10 @@ public class MainProcess {
             System.out.print("비밀번호 : ");
             String password = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌의 비밀번호에 맞게 입력시 true , 없을시 재입력
             int exact = accountList.passwordCorrection(index, password);
             if (exact == 1) {
+                //선택한 계좌 , 거래내역 조회
                 accountList.getHistory(accountNum);
                 break;
             } else {
@@ -198,6 +223,7 @@ public class MainProcess {
         }
     }
 
+    //계좌 입금 기능
     public void depositProcess() {
         int index;
         while (true) {
@@ -205,6 +231,7 @@ public class MainProcess {
             System.out.print("계좌번호 : ");
             String accountNum = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌에 맞게 계좌번호 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -213,11 +240,12 @@ public class MainProcess {
                 break;
             }
         }
-        int nAmount;
+        int nAmount; // 입금 금액 변수
         while (true) {
             System.out.print("비밀번호 : ");
             String password = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌의 비밀번호 맞게 입력시 true , 없을시 재입력
             int exact = accountList.passwordCorrection(index, password);
             if (exact !=1) {
                 System.out.println("비밀번호가 일치하지 않습니다.");
@@ -230,9 +258,12 @@ public class MainProcess {
                 try {
                     String money = scanner.nextLine();
                     nAmount = Integer.parseInt(money);
+                    //선택한 계좌 입금 기능
                     accountList.deposit(index, nAmount);
                     break;
-                } catch (NumberFormatException e) {
+                } 
+                // 금액에 문자입력시 예외처리
+                catch (NumberFormatException e) {
                     System.out.println("금액 입력이 올바르지 않습니다.");
                 }
             }
@@ -240,6 +271,7 @@ public class MainProcess {
         }
     }
 
+    //계좌 출금 기능
     public void withdrawalProcess() {
         int index;
         while (true) {
@@ -247,6 +279,7 @@ public class MainProcess {
             System.out.print("계좌번호 : ");
             String accountNum = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌에 맞게 계좌번호 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -261,6 +294,7 @@ public class MainProcess {
             System.out.print("비밀번호 : ");
             String password = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌의 비밀번호로 맞게 입력시 true , 없을시 재입력
             int exact = accountList.passwordCorrection(index, password);
             if (exact !=1) {
                 System.out.println("비밀번호가 일치하지 않습니다.");
@@ -272,9 +306,12 @@ public class MainProcess {
                 try {
                     String money = scanner.nextLine();
                     nAmount = Integer.parseInt(money);
+                    // 출금 기능
                     accountList.withdrawal(index, nAmount);
                     break;
-                } catch (NumberFormatException e) {
+                }
+                // 금액에 문자입력시 예외처리
+                catch (NumberFormatException e) {
                     System.out.println("금액 입력이 올바르지 않습니다.");
                 }
             }
@@ -282,12 +319,14 @@ public class MainProcess {
         }
     }
 
+    // 계좌 송금 기능
     public void remittanceProcess() {
         int index;
         while (true) {
             System.out.print("계좌번호 : ");
             String accountNum = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌에 맞게 계좌번호 입력시 true , 없을시 재입력
             index = accountList.getIndexByAccountNum(accountNum);
             if (index == -1) {
                 System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -302,6 +341,7 @@ public class MainProcess {
             System.out.print("비밀번호 : ");
             String password = scanner.nextLine();
             System.out.println();
+            // 저장된 계좌에 맞게 비밀번호 입력시 true , 없을시 재입력
             int exact = accountList.passwordCorrection(index, password);
             if (exact !=1) {
                 System.out.println("비밀번호가 일치하지 않습니다.");
@@ -312,6 +352,7 @@ public class MainProcess {
                 System.out.println("받으실 분의 계좌번호를 입력하세요.");
                 System.out.print("계좌번호 : ");
                 receiveAccountNum = scanner.nextLine();
+                // 송금할 계좌가 있는 계좌라면 true , 없을시 재입력
                 receiveIndex = accountList.getIndexByAccountNum(receiveAccountNum);
                 if (receiveIndex == -1) {
                     System.out.println("계좌번호를 찾을 수 없습니다.");
@@ -326,7 +367,9 @@ public class MainProcess {
                     accountList.withdrawal(index, nAmount);
                     accountList.deposit(receiveIndex, nAmount);
                     break;
-                } catch (NumberFormatException e) {
+                } 
+                // 금액입력에 문자입력시 예외처리
+                catch (NumberFormatException e) {
                     System.out.println("금액 입력이 올바르지 않습니다.");
                 }
             }
@@ -334,6 +377,7 @@ public class MainProcess {
         }
     }
 
+    //계좌 잔고 확인 기능
     public void getAmountProcess() {
         int index;
         // 계좌번호 입력 후 AccountList에서 입력한 계좌번호의 계좌정보 리스트의 인덱스를 받는다.
@@ -356,6 +400,7 @@ public class MainProcess {
             System.out.println();
             int exact = accountList.passwordCorrection(index, password);
             if (exact == 1) {
+                //선택한 계좌 잔고 확인
                 accountList.getAmount(index);
                 break;
             } else {

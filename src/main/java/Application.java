b4436efd7a0f3db21e0b25681c3c4ce8fb8d8ace
HanJungkyu <jk891113@naver.com>
2022-11-28 16:25:50 +0900

    미사용 코드 정리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -14,32 +14,6 @@ public class Application {
         MainProcess mainProcess = new MainProcess();
         Scanner scanner = new Scanner(System.in);
 
-//        while (true) {
-//            mainUI.run();
-//            try {
-//                System.out.print("번호 입력 : ");
-//                String sChooseMain = scanner.nextLine();
-//                int chooseMain = Integer.parseInt(sChooseMain);
-//                if (chooseMain == 1) {
-//                    mainUI.createAccount();
-//                } else if (chooseMain == 2) {
-//                    mainUI.manageAccount();
-//                } else if (chooseMain == 3) {
-//                    mainUI.checkAccount();
-//                } else if (chooseMain == 4) {
-//                    mainUI.checkAmount();
-//                } else if (chooseMain == 5) {
-//                    mainUI.depositWithdrawal();
-//                } else if (chooseMain == 0) {
-//                    System.out.println("프로그램을 종료합니다.");
-//                    System.exit(0);
-//                } else {
-//                    System.out.println("잘못된 명령어입니다.");
-//                }
-//            } catch (NumberFormatException e) {
-//                System.out.println("입력이 올바르지 않습니다.");
-//            }
-//        }
         while (true) {
             //첫 UI 출력 (0~5의 각기능 표기)
             mainUI.run();
